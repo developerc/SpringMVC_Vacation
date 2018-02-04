de6d207494c1f2d0d6a34acbe2d3a34afb3c63ba
bliz <developerc@yandex.ru>
2018-02-04 21:46:03 +0300

    css change

---
+++ src/main/java/ru/home/saperov/Sheduler.java
@@ -0,0 +1,317 @@
+package ru.home.saperov;
+
+public class Sheduler {
+    private String jan1;
+    private String jan2;
+    private String jan3;
+    private String jan4;
+    private String jan5;
+    private String jan6;
+    private String jan7;
+    private String jan8;
+    private String jan9;
+    private String jan10;
+    private String jan11;
+    private String jan12;
+    private String jan13;
+    private String jan14;
+    private String jan15;
+    private String jan16;
+    private String jan17;
+    private String jan18;
+    private String jan19;
+    private String jan20;
+    private String jan21;
+    private String jan22;
+    private String jan23;
+    private String jan24;
+    private String jan25;
+    private String jan26;
+    private String jan27;
+    private String jan28;
+    private String jan29;
+    private String jan30;
+    private String jan31;
+
+    public Sheduler(String jan1, String jan2, String jan3, String jan4, String jan5, String jan6, String jan7, String jan8, String jan9, String jan10, String jan11, String jan12, String jan13, String jan14, String jan15, String jan16, String jan17, String jan18, String jan19, String jan20, String jan21, String jan22, String jan23, String jan24, String jan25, String jan26, String jan27, String jan28, String jan29, String jan30, String jan31) {
+        this.jan1 = jan1;
+        this.jan2 = jan2;
+        this.jan3 = jan3;
+        this.jan4 = jan4;
+        this.jan5 = jan5;
+        this.jan6 = jan6;
+        this.jan7 = jan7;
+        this.jan8 = jan8;
+        this.jan9 = jan9;
+        this.jan10 = jan10;
+        this.jan11 = jan11;
+        this.jan12 = jan12;
+        this.jan13 = jan13;
+        this.jan14 = jan14;
+        this.jan15 = jan15;
+        this.jan16 = jan16;
+        this.jan17 = jan17;
+        this.jan18 = jan18;
+        this.jan19 = jan19;
+        this.jan20 = jan20;
+        this.jan21 = jan21;
+        this.jan22 = jan22;
+        this.jan23 = jan23;
+        this.jan24 = jan24;
+        this.jan25 = jan25;
+        this.jan26 = jan26;
+        this.jan27 = jan27;
+        this.jan28 = jan28;
+        this.jan29 = jan29;
+        this.jan30 = jan30;
+        this.jan31 = jan31;
+    }
+
+    public String getJan1() {
+        return jan1;
+    }
+
+    public void setJan1(String jan1) {
+        this.jan1 = jan1;
+    }
+
+    public String getJan2() {
+        return jan2;
+    }
+
+    public void setJan2(String jan2) {
+        this.jan2 = jan2;
+    }
+
+    public String getJan3() {
+        return jan3;
+    }
+
+    public void setJan3(String jan3) {
+        this.jan3 = jan3;
+    }
+
+    public String getJan4() {
+        return jan4;
+    }
+
+    public void setJan4(String jan4) {
+        this.jan4 = jan4;
+    }
+
+    public String getJan5() {
+        return jan5;
+    }
+
+    public void setJan5(String jan5) {
+        this.jan5 = jan5;
+    }
+
+    public String getJan6() {
+        return jan6;
+    }
+
+    public void setJan6(String jan6) {
+        this.jan6 = jan6;
+    }
+
+    public String getJan7() {
+        return jan7;
+    }
+
+    public void setJan7(String jan7) {
+        this.jan7 = jan7;
+    }
+
+    public String getJan8() {
+        return jan8;
+    }
+
+    public void setJan8(String jan8) {
+        this.jan8 = jan8;
+    }
+
+    public String getJan9() {
+        return jan9;
+    }
+
+    public void setJan9(String jan9) {
+        this.jan9 = jan9;
+    }
+
+    public String getJan10() {
+        return jan10;
+    }
+
+    public void setJan10(String jan10) {
+        this.jan10 = jan10;
+    }
+
+    public String getJan11() {
+        return jan11;
+    }
+
+    public void setJan11(String jan11) {
+        this.jan11 = jan11;
+    }
+
+    public String getJan12() {
+        return jan12;
+    }
+
+    public void setJan12(String jan12) {
+        this.jan12 = jan12;
+    }
+
+    public String getJan13() {
+        return jan13;
+    }
+
+    public void setJan13(String jan13) {
+        this.jan13 = jan13;
+    }
+
+    public String getJan14() {
+        return jan14;
+    }
+
+    public void setJan14(String jan14) {
+        this.jan14 = jan14;
+    }
+
+    public String getJan15() {
+        return jan15;
+    }
+
+    public void setJan15(String jan15) {
+        this.jan15 = jan15;
+    }
+
+    public String getJan16() {
+        return jan16;
+    }
+
+    public void setJan16(String jan16) {
+        this.jan16 = jan16;
+    }
+
+    public String getJan17() {
+        return jan17;
+    }
+
+    public void setJan17(String jan17) {
+        this.jan17 = jan17;
+    }
+
+    public String getJan18() {
+        return jan18;
+    }
+
+    public void setJan18(String jan18) {
+        this.jan18 = jan18;
+    }
+
+    public String getJan19() {
+        return jan19;
+    }
+
+    public void setJan19(String jan19) {
+        this.jan19 = jan19;
+    }
+
+    public String getJan20() {
+        return jan20;
+    }
+
+    public void setJan20(String jan20) {
+        this.jan20 = jan20;
+    }
+
+    public String getJan21() {
+        return jan21;
+    }
+
+    public void setJan21(String jan21) {
+        this.jan21 = jan21;
+    }
+
+    public String getJan22() {
+        return jan22;
+    }
+
+    public void setJan22(String jan22) {
+        this.jan22 = jan22;
+    }
+
+    public String getJan23() {
+        return jan23;
+    }
+
+    public void setJan23(String jan23) {
+        this.jan23 = jan23;
+    }
+
+    public String getJan24() {
+        return jan24;
+    }
+
+    public void setJan24(String jan24) {
+        this.jan24 = jan24;
+    }
+
+    public String getJan25() {
+        return jan25;
+    }
+
+    public void setJan25(String jan25) {
+        this.jan25 = jan25;
+    }
+
+    public String getJan26() {
+        return jan26;
+    }
+
+    public void setJan26(String jan26) {
+        this.jan26 = jan26;
+    }
+
+    public String getJan27() {
+        return jan27;
+    }
+
+    public void setJan27(String jan27) {
+        this.jan27 = jan27;
+    }
+
+    public String getJan28() {
+        return jan28;
+    }
+
+    public void setJan28(String jan28) {
+        this.jan28 = jan28;
+    }
+
+    public String getJan29() {
+        return jan29;
+    }
+
+    public void setJan29(String jan29) {
+        this.jan29 = jan29;
+    }
+
+    public String getJan30() {
+        return jan30;
+    }
+
+    public void setJan30(String jan30) {
+        this.jan30 = jan30;
+    }
+
+    public String getJan31() {
+        return jan31;
+    }
+
+    public void setJan31(String jan31) {
+        this.jan31 = jan31;
+    }
+}
