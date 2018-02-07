e3d6f601cc0d5190790c4c20ab3fd2c4db64e8fb
bliz <developerc@yandex.ru>
2018-02-07 21:58:15 +0300

    add repData class

--- src/main/java/ru/home/saperov/RepData.java
+++ src/main/java/ru/home/saperov/RepData.java
@@ -1,4 +1,42 @@
 package ru.home.saperov;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@ManagedBean(name="repData", eager = true)
+@SessionScoped
 public class RepData {
+    private ArrayList<String> repStr = new ArrayList<String>();
+    private ArrayList<ArrayList<String>> repListStr = new ArrayList<ArrayList<String>>();
+
+    public ArrayList<ArrayList<String>> getRepListStr() {
+        repListStr.add(getRepStr());
+        repListStr.add(getRepStr());
+        return repListStr;
+    }
+
+    public void setRepListStr(ArrayList<ArrayList<String>> repListStr) {
+        this.repListStr = repListStr;
+    }
+
+    public ArrayList<String> getRepStr() {
+        repStr.add("one");
+        repStr.add("two");
+        repStr.add("three");
+
+        return repStr;
+    }
+
+    public void setRepStr(ArrayList<String> repStr) {
+        this.repStr = repStr;
+    }
+
+
+    /*<h:form>
+           <c:forEach items="#{repData.repStr}"  var="item">
+                <h:outputText value="#{item}" />
+           </c:forEach>
+       </h:form>*/
 }
